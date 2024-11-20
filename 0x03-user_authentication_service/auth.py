bfa35c13d2bebbaf0a615935d73f10b958b6c135
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-11-20 10:55:00 +0200

    added auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,8 @@ from bcrypt import hashpw, gensalt
 
 
 def _hash_password(hashed_password: str) -> bytes:
+    """this function returns a hashed password
+    """
     bytes = hashed_password.encode('utf-8')
     salt = gensalt()
     return hashpw(bytes, salt)
