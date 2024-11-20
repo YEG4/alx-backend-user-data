86232ff58a978cb0a08441dc9f849b7afb1cd53b
YEG4 <ahmed.t.ghoneim@gmail.com>
2024-11-20 10:35:52 +0200

    added update_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -74,7 +74,7 @@ class DB:
         try:
             user = self.find_user_by(id=userId)
         except NoResultFound:
-            return None
+            return ValueError()
         for key, value in kwargs.items():
             if hasattr(User, key):
                 setattr(user, key, value)
